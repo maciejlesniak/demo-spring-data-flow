0acbc47279720ac5fcc6a53be704e6cada173c99
Maciek Leśniak <maciek@sparkmedia.pl>
2024-07-24 23:13:07 +0200

    testing

--- demo-processor-kafka/src/test/java/pl/sparkidea/springcloud/demo/demoprocessor/DemoProcessorKafkaApplicationTests.java
+++ demo-processor-kafka/src/test/java/pl/sparkidea/springcloud/demo/demoprocessor/DemoProcessorKafkaApplicationTests.java
@@ -1,13 +1,38 @@
 package pl.sparkidea.springcloud.demo.demoprocessor;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.binder.test.EnableTestBinder;
+import org.springframework.cloud.stream.binder.test.InputDestination;
+import org.springframework.cloud.stream.binder.test.OutputDestination;
+import org.springframework.messaging.support.GenericMessage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@EnableTestBinder
 class DemoProcessorKafkaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private InputDestination input;
+
+    @Autowired
+    private OutputDestination output;
+
+
+    @Test
+    void processorFunction_shouldUpecaseAndAddStaticString_whenStringGiven() {
+
+        // given
+        var inputMessage = new GenericMessage<>("hello");
+
+        // when
+        input.send(inputMessage);
+        var receivedMessageBytes = output.receive().getPayload();
+
+        // then
+        assertEquals("HELLO -- additional string in lower case", new String(receivedMessageBytes));
+    }
 
 }
--- demo-source-kafka/src/test/java/pl/sparkidea/springcloud/demo/sourcekafka/DemoSourceKafkaApplicationTests.java
+++ demo-source-kafka/src/test/java/pl/sparkidea/springcloud/demo/sourcekafka/DemoSourceKafkaApplicationTests.java
@@ -1,13 +1,28 @@
 package pl.sparkidea.springcloud.demo.sourcekafka;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.binder.test.EnableTestBinder;
+import org.springframework.cloud.stream.binder.test.OutputDestination;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@EnableTestBinder
 class DemoSourceKafkaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private OutputDestination output;
+
+    @Test
+    void sourceFunction_shouldProduceDemoMessage() {
+
+        // when
+        var receivedMessageBytes = output.receive().getPayload();
+
+        // then
+        assertTrue(new String(receivedMessageBytes).startsWith("Demo message; ts:"));
+    }
 
 }
